392af2890944b078a64a8cb3ed02862549d24a3e
yurkiww <yurkiw.nazar@gmail.com>
2021-09-27 00:19:09 +0300

    HW2 done

---
+++ HW2/CaseChanger/my.js
@@ -0,0 +1,14 @@
+function changeCase(str){
+    let changedStr = '';
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i].toLowerCase()) {
+            changedStr += str[i].toUpperCase();
+        } else if (str[i] === str[i].toUpperCase()) {
+            changedStr += str[i].toLowerCase()
+        } 
+    }
+    console.log(changedStr)
+}
+
+
---
+++ HW2/CrashScript/my.js
@@ -0,0 +1,10 @@
+
+// while(true){
+//     console.log('bob')
+// }
+
+let num = 1;
+while(true){
+    num = num + num;  
+}
+
